06d722768da9f1f0b1e284b8fc7d1b3507fccba3
tdimnet <thomas.dimnet@gmail.com>
2024-08-12 14:50:52 +0200

    Adding color theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,13 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
+    colors: {
+      'dark-blue': '#262E38',
+      'light-grey': '#969FAD',
+      'orange': '#FC7614',
+      'pure-white': '#FFF',
+      'very-dark-blue': '#131518'
+    },
     extend: {},
   },
   plugins: [],
